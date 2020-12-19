9361b2512fd846d4c92a78d1f9a385e6d8ef9b5b
Eldus <arsik2001@gmail.com>
2020-12-19 07:43:24 +0200

    final final commit

--- src/com/eldus/Main.java
+++ src/com/eldus/Main.java
@@ -12,10 +12,60 @@ public class Main {
         return d;
     }
 
+//    public static String stringInventer1(String s) {
+//        StringBuilder stringBuilder = new StringBuilder();
+//        char[] chars = s.toCharArray();
+//        for (int i = 0; i < chars.length; i++) {
+//            if (Character.isLetter(chars[i])) {
+//                if (Character.isLowerCase(chars[i])) {
+//                    chars[i] = Character.toUpperCase(chars[i]);
+//                } else {
+//                    chars[i] = Character.toLowerCase(chars[i]);
+//                }
+//            }
+//            stringBuilder.append(chars[i]);
+//        }
+//        return stringBuilder.toString();
+//    }
+
+    public static String stringInventer(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetter(s.charAt(i))) {
+                if (Character.isLowerCase(s.charAt(i))) {
+                    stringBuilder.append(Character.toUpperCase(s.charAt(i)));
+                } else {
+                    stringBuilder.append(Character.toLowerCase(s.charAt(i)));
+                }
+            } else {
+                stringBuilder.append(s.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String stringBlaBlaBla(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (s.length() % 2 == 0) {
+            stringBuilder.append(s.substring(s.length()/2));
+            stringBuilder.append(s.substring(0,s.length()/2));
+        }
+        else{
+            stringBuilder.append(s.substring(s.length()/2+1));
+            stringBuilder.append(s.charAt(s.length()/2));
+            stringBuilder.append(s.substring(0,s.length()/2));
+        }
+        return stringBuilder.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println(centerOfString("abcde"));
         System.out.println(centerOfString("abcd"));
 
+        System.out.println(stringInventer("AbCd[]kK"));
+
+        System.out.println(stringBlaBlaBla("AbCd"));
+        System.out.println(stringBlaBlaBla("AbCdE"));
 
         Bottle a1 = new Bottle(2, 0.4, Color.GREEN, true);
         Bottle a2 = new Bottle(2, 0.4, Color.GREEN, true);
